43cde9ed2126464399cb6eeb3c9dce97bff7594a
marbi <marbi@DESKTOP-IK54SCE>
2017-07-27 16:10:57 -0400

    Non-Recursive Binary Search

---
+++ src/BinarySearch.java
@@ -0,0 +1,32 @@
+
+public class BinarySearch {
+
+    public static boolean search(int[] array, int searchedValue) {
+        int beginning = 0;
+        int end = array.length - 1;
+
+        while (beginning <= end) {
+            int middle = (beginning + end) / 2;
+            if (array[middle] == searchedValue) {
+                return true;
+            }
+
+            if (searchedValue > array[middle]) {
+                beginning = middle + 1;
+                middle = (beginning + end) / 2;
+                if (array[middle] == searchedValue) {
+                    return true;
+                }
+            }
+
+            if (searchedValue < array[middle]) {
+                end = middle;
+                middle = (beginning + end) / 2;
+                if (array[middle] == searchedValue) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
